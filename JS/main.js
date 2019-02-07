2ad0ea63229c41a63edf14d5b7d6671c5487f38c
Devis <dreamscape8991@gmail.com>
2019-02-07 13:30:31 +0100

    No Javascript

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+const addition = document.getElementById("addition");
+const subtraction = document.getElementById("subtraction");
+const multiplication = document.getElementById("multiplication");
+const division = document.getElementById("division");
+const modulus = document.getElementById("modulus")
+const dot = document.getElementById("dot");
+const equal = document.getElementById("equal");
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const backspace = document.getElementById("backspace");
+const clear = document.getElementById("clear");
+const resultsDiv = document.getElementById("results-div");
+let number;
+let firstNumber;
+let secondNumber;
+let operator;
+let elementId;
+
