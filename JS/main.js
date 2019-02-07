023f5a821e8c144c00223ae918f3b12fe4e57eb8
Devis <dreamscape8991@gmail.com>
2019-02-08 00:56:00 +0100

    FINISHED

--- JS/main.js
+++ JS/main.js
@@ -18,9 +18,151 @@ const nine = document.getElementById("nine");
 const backspace = document.getElementById("backspace");
 const clear = document.getElementById("clear");
 const resultsDiv = document.getElementById("results-div");
+
 let number;
-let firstNumber;
 let secondNumber;
-let operator;
-let elementId;
+let operation;
+
+
+function add(num1, num2) {
+    resultsDiv.innerHTML =  parseFloat(num1) + parseFloat(num2);
+}
+
+function subtract(num1, num2) {
+    resultsDiv.innerHTML =  num1 - num2;
+}
+
+function multiply(num1, num2) {
+    resultsDiv.innerHTML =  num1 * num2;
+}
+
+function divide(num1, num2) {
+    resultsDiv.innerHTML =  num1 / num2;
+    if(num2 == 0) {
+        resultsDiv.innerHTML = "ERROR!";
+    }
+}
+
+function modul(num1, num2) {
+    resultsDiv.innerHTML =  num1 % num2;
+}
+
+
+zero.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 0;
+    }
+});
+one.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 1;
+    }
+});
+two.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 2;
+    }
+});
+three.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 3;
+    }
+});
+four.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 4;
+    }
+});
+five.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 5;
+    }
+});
+six.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 6;
+    }
+});
+seven.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 7;
+    }
+});
+eight.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 8;
+    }
+});
+nine.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += 9;
+    }
+});
+
+dot.addEventListener("click", function() {
+    if(resultsDiv.innerHTML.length < 9) {
+        resultsDiv.innerHTML += ".";
+    }
+});
+
+addition.addEventListener("click", function() {
+    number = resultsDiv.innerHTML;
+    resultsDiv.innerHTML = "";
+    operation = "+";
+});
+
+subtraction.addEventListener("click", function() {
+    number = resultsDiv.innerHTML;
+    resultsDiv.innerHTML = "";
+    operation = "-";
+});
+
+division.addEventListener("click", function() {
+    number = resultsDiv.innerHTML;
+    resultsDiv.innerHTML = "";
+    operation = "/";
+});
+
+multiplication.addEventListener("click", function() {
+    number = resultsDiv.innerHTML;
+    resultsDiv.innerHTML = "";
+    operation = "*";
+});
+
+modulus.addEventListener("click", function() {
+    number = resultsDiv.innerHTML;
+    resultsDiv.innerHTML = "";
+    operation = "%";
+});
+
+
+function operate(operator, num1, num2) {
+    if(operator == "+") {
+        add(num1, num2);
+    }else if(operator == "-") {
+        subtract(num1, num2);
+    }else if(operator == "*") {
+        multiply(num1, num2);
+    }else if(operator == "/") {
+        divide(num1, num2);
+    }else if(operator == "%") {
+        modul(num1, num2);
+    }
+}
+
+equal.addEventListener("click", function() {
+    secondNumber = resultsDiv.innerHTML;
+    operate(operation, number, secondNumber);
+    if(resultsDiv.innerHTML.length > 9) {
+        resultsDiv.innerHTML = resultsDiv.innerHTML.slice(0,10);
+    }
+});
+
+clear.addEventListener("click", function() {
+    resultsDiv.innerHTML = "";
+});
+
+backspace.addEventListener("click", function() {
+    resultsDiv.innerHTML = resultsDiv.innerHTML.slice(0,-1);
+});
 
